260c7cd051e36de67827c8ed06687c9f054b2789
ilotterytea <me@ilotterytea.kz>
2024-05-09 18:13:52 +0500

    feat: validaing the migration directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <filesystem>
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main(int argc, char *argv[]) {
   std::cout << "hi!" << std::endl;
@@ -18,6 +20,23 @@ int main(int argc, char *argv[]) {
   if (!directory_exists) {
     std::cout << "Creating the \"" << folder_path << "\" directory...\n";
     std::filesystem::create_directories(folder_path);
+  }
+
+  // validating directories
+  std::vector<std::string> valid_directory_paths;
+  for (const auto &entry : std::filesystem::directory_iterator(folder_path)) {
+    std::string path = entry.path();
+
+    bool up_exists = std::filesystem::exists(path + "/up.sql");
+    bool down_exists = std::filesystem::exists(path + "/down.sql");
+
+    if (up_exists && down_exists) {
+      valid_directory_paths.push_back(path);
+    } else {
+      std::cout << "Ignoring the \"" << path
+                << "\" folder because it doesn't have either up.sql, or "
+                   "down.sql...\n";
+    }
   }
 
   return 0;
